0a1818889d8096f2084a55d95a36cdb14d78ed85
Fredo Erxleben <fredo.erxleben@tu-dresden.de>
2018-02-21 19:47:36 +0100

    Properly defined an AtomicDateTimeWidget and field
    * Still needs correct naming for CSS

--- fpga_manager/forms/create_reservation.py
+++ fpga_manager/forms/create_reservation.py
@@ -2,37 +2,83 @@ from django.forms import DateTimeField
 from django.forms import IntegerField
 from django.forms import ModelChoiceField
 from django.forms import ModelForm
+from django.forms import MultiValueField
+from django.forms import MultiWidget
+from django.forms import NumberInput
 from django.utils import timezone
 
 from fpga_manager.models import RegionType
 from fpga_manager.models import VFpga
 
 
-class CreateReservationForm(ModelForm):
-    class Meta:
-        model = VFpga
-        fields = ['reservation_start_date', 'reservation_end_date']
+class AtomicDateTimeWidget(MultiWidget):
+    def __init__(self, *args, **kwargs):
+        self.widgets = [
+            NumberInput(
+                attrs={"required": True,
+                       "min": timezone.now().year,
+                       "max": 9999,
+                       "size": 4}
+            ),  # year
+            NumberInput(
+                attrs={"required": True,
+                       "min": 1,
+                       "max": 12,
+                       "size": 2}
+            ),  # month
+            NumberInput(
+                attrs={"required": True,
+                       "min": 1,
+                       "max": 31,
+                       "size": 2}
+            ),  # day
+            NumberInput(
+                attrs={"required": True,
+                       "min": 0,
+                       "max": 59,
+                       "size": 2}
+            ),  # hour
+            NumberInput(
+                attrs={"required": True,
+                       "min": 0,
+                       "max": 59,
+                       "size": 2}
+            ),  # minute
+        ]
+        super(AtomicDateTimeWidget, self).__init__(self.widgets, *args, **kwargs)
 
-    reservation_start_date = DateTimeField(
-        required=True,
-        initial=timezone.now
-    )
+    def decompress(self, value):
 
-    reservation_start_date_year = IntegerField(
+        if value:
+            datetime = value
+        else:
+            datetime = timezone.now()
+
+        return [datetime.year,
+                datetime.month,
+                datetime.day,
+                datetime.hour,
+                datetime.minute]
+
+
+class AtomicDateTimeField(MultiValueField):
+    widget = AtomicDateTimeWidget
+
+    field_year = IntegerField(
         required=True,
         initial=timezone.now().year,
         min_value=timezone.now().year,
         max_value=9999,
     )
 
-    reservation_start_date_month = IntegerField(
+    field_month = IntegerField(
         required=True,
         initial=timezone.now().month,
         min_value=1,
         max_value=12,
     )
 
-    reservation_start_date_day = IntegerField(
+    field_day = IntegerField(
         required=True,
         initial=timezone.now().day,
         min_value=1,
@@ -40,19 +86,46 @@ class CreateReservationForm(ModelForm):
 
     )
 
-    reservation_start_date_hour = IntegerField(
+    field_hour = IntegerField(
         required=True,
         initial=timezone.now().hour,
         min_value=0,
         max_value=23,
     )
 
-    reservation_start_date_minute = IntegerField(
+    field_minute = IntegerField(
         required=True,
         initial=timezone.now().minute,
         min_value=0,
         max_value=59,
     )
+
+    def __init__(self, *args, **kwargs):
+        super(AtomicDateTimeField, self).__init__(*args, **kwargs)
+        self.fields = [
+            self.field_year,
+            self.field_month,
+            self.field_day,
+            self.field_hour,
+            self.field_minute
+        ]
+
+    def compress(self, data_list):
+        return timezone.datetime(
+            year=data_list[0],
+            month=data_list[1],
+            day=data_list[2],
+            hour=data_list[3],
+            minute=data_list[4]
+        )
+
+
+class CreateReservationForm(ModelForm):
+    class Meta:
+        model = VFpga
+        fields = ['reservation_start_date', 'reservation_end_date']
+
+    reservation_start_date = AtomicDateTimeField()
 
     reservation_end_date = DateTimeField(
         required=True,
